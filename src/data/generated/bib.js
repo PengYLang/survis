5a1e45d0add598384ac5c8d290095919455fd4ff
YL-oss <fttlxxx@gmail.com>
2025-05-14 19:05:26 +0100

    Update all literature data for GitHub Pages

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,94 @@
 ﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+    "Alqobali2024SemanticNavigation": {
+        "author": "Alqobali, Raghad and Alshmrani, Maha and Alnasser, Reem and Rashidi, Asrar and Alhmiedat, Tareq and Alia, Osama Moh'd",
+        "journal": "Applied Sciences",
+        "number": "1",
+        "pages": "89",
+        "publisher": "MDPI",
+        "title": "A Survey on Robot Semantic Navigation Systems for Indoor Environments",
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
+        "volume": "14",
+        "year": "2024"
+    },
+    "Chaplot2020ObjectGoalNavigation": {
+        "author": "Chaplot, Devendra Singh and Gandhi, Dhiraj and Gupta, Abhinav and Salakhutdinov, Ruslan",
+        "booktitle": "Advances in Neural Information Processing Systems (NeurIPS)",
+        "publisher": "NeurIPS",
+        "title": "Object Goal Navigation using Goal-Oriented Semantic Exploration",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "Chen2017MV3D": {
+        "author": "Chen, Xiaozhi and Ma, Huimin and Wan, Ji and Li, Bo and Xia, Tian",
+        "booktitle": "Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "pages": "1907--1915",
+        "title": "Multi-View 3D Object Detection Network for Autonomous Driving",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "Cockburn2008MemoryBounded": {
+        "author": "Cockburn, Denton and Kobti, Ziad",
+        "booktitle": "Proceedings of the Twenty-First International FLAIRS Conference",
+        "pages": "376--381",
+        "publisher": "AAAI",
+        "title": "Memory-Bounded D* Lite",
+        "type": "inproceedings",
+        "year": "2008"
+    },
+    "Hart1968Formal": {
+        "author": "Hart, Peter E. and Nilsson, Nils J. and Raphael, Bertram",
+        "journal": "IEEE Transactions on Systems Science and Cybernetics",
+        "number": "2",
+        "pages": "100--107",
+        "publisher": "IEEE",
+        "title": "A Formal Basis for the Heuristic Determination of Minimum Cost Paths",
+        "type": "article",
+        "volume": "SSC-4",
+        "year": "1968"
+    },
+    "Hutter2016ANYmal": {
+        "address": "Daejeon, Korea",
+        "author": "Hutter, Marco and Gehring, Christian and Jud, Dominic and Lauber, Andreas and Bellicoso, C. Dario and Tsounis, Vassilios and Hwangbo, Jemin and Bodie, Karen and Fankhauser, Peter and Bloesch, Michael and Diethelm, Remo and Bachmann, Samuel and Melzer, Amir and Hoepflinger, Mark",
+        "booktitle": "Proceedings of IEEE/RSJ International Conference on Intelligent Robots and Systems (IROS)",
+        "pages": "38--44",
+        "publisher": "IEEE",
+        "title": "ANYmal - A Highly Mobile and Dynamic Quadrupedal Robot",
+        "type": "inproceedings",
         "year": "2016"
+    },
+    "MultimodalHybrid2023": {
+        "author": "Unknown",
+        "note": "Document provided by user",
+        "title": "A Multimodal Hybrid Late-Cascade Fusion Network for Enhanced 3D Object Detection",
+        "type": "article",
+        "year": "2023"
+    },
+    "Rollo2023ArtifactsMapping": {
+        "author": "Rollo, Federico and Raiola, Gennaro and Zunino, Andrea and Tsagarakis, Nikolaos and Ajoudani, Arash",
+        "booktitle": "European Conference on Mobile Robots (ECMR)",
+        "publisher": "IEEE",
+        "title": "Artifacts Mapping: Multi-Modal Semantic Mapping for Object Detection and 3D Localization",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "Scicluna2024RobustPerception": {
+        "author": "Scicluna, Adam and Le Gentil, Cedric and Sutjipto, Sheila and Paul, Gavin",
+        "booktitle": "IEEE International Conference on Automation Science and Engineering (CASE)",
+        "pages": "920--926",
+        "publisher": "IEEE",
+        "title": "Towards Robust Perception for Assistive Robotics: An RGB-Event-LiDAR Dataset and Multi-Modal Detection Pipeline",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "Thrun2000Probabilistic": {
+        "author": "Thrun, Sebastian",
+        "journal": "AI Magazine",
+        "number": "4",
+        "pages": "93--109",
+        "publisher": "AAAI",
+        "title": "Probabilistic Algorithms in Robotics",
+        "type": "article",
+        "volume": "21",
+        "year": "2000"
     }
 };
